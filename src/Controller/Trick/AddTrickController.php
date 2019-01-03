23beb36c9e238ec74a2463460289a7b42fc94c1a
ludovicjj <jahanlud@gmail.com>
2019-01-04 00:24:25 +0100

    Create method add() and route

--- src/Controller/Trick/AddTrickController.php
+++ src/Controller/Trick/AddTrickController.php
@@ -3,7 +3,28 @@
 namespace App\Controller\Trick;
 
 
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
 class AddTrickController
 {
+    /**
+     * @var Environment
+     */
+    private $twig;
 
+    public function __construct(
+        Environment $twig
+    )
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/figure/ajouter", name="add_trick")
+     */
+    public function add()
+    {
+
+    }
 }
