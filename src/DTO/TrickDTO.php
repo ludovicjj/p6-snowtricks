7081ae0b98ac0a126ba4b69b4faba83d7db89b32
ludovicjj <jahanlud@gmail.com>
2019-01-05 00:11:54 +0100

    Update TrickDTO with field videos

--- src/DTO/TrickDTO.php
+++ src/DTO/TrickDTO.php
@@ -21,14 +21,21 @@ class TrickDTO
      */
     public $category;
 
+    /**
+     * @var array
+     */
+    public $videos;
+
     public function __construct(
         string $title = null,
         string $description = null,
-        Category $category = null
+        Category $category = null,
+        array $videos = null
     )
     {
         $this->title = $title;
         $this->description = $description;
         $this->category = $category;
+        $this->videos = $videos;
     }
 }
