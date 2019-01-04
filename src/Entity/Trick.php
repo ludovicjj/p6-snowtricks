551b541b7d4c9654b2ea2641248c619b45194cf4
ludovicjj <jahanlud@gmail.com>
2019-01-04 03:12:38 +0100

    Create Trick Entity

---
+++ src/Entity/Trick.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+use App\Service\Slugger;
+
+class Trick
+{
+    /**
+     * @var UuidInterface
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $slug;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * @var Category
+     */
+    private $category;
+
+
+    /**
+     * Trick constructor.
+     * @param string $title
+     * @param string $description
+     * @param string $slug
+     * @param Category $category
+     * @throws \Exception
+     */
+    public function __construct(
+        string $title,
+        string $description,
+        string $slug,
+        Category $category
+    )
+    {
+        $this->id = Uuid::uuid4();
+        $this->title = $title;
+        $this->description = $description;
+        $this->slug = Slugger::makeSlug($slug);
+        $this->createdAt = new \DateTime();
+        $this->category = $category;
+    }
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function getCategory(): Category
+    {
+        return $this->category;
+    }
+}
