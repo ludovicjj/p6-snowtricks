9978b81b291bd7dcb4f0407cceb50b0283a44cf9
ludovicjj <jahanlud@gmail.com>
2019-01-04 05:39:45 +0100

    If addTrickHandler return true redirect to home page

--- src/Controller/Trick/AddTrickController.php
+++ src/Controller/Trick/AddTrickController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Trick;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use App\Form\Handler\AddTrickHandler;
 use App\Form\Type\AddTrickType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,14 +24,34 @@ class AddTrickController
      */
     private $formFactory;
 
+    /**
+     * @var AddTrickHandler
+     */
+    private $addTrickHandler;
 
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * AddTrickController constructor.
+     * @param Environment $twig
+     * @param FormFactoryInterface $formFactory
+     * @param AddTrickHandler $addTrickHandler
+     * @param UrlGeneratorInterface $urlGenerator
+     */
     public function __construct(
         Environment $twig,
-        FormFactoryInterface $formFactory
+        FormFactoryInterface $formFactory,
+        AddTrickHandler $addTrickHandler,
+        UrlGeneratorInterface $urlGenerator
     )
     {
         $this->twig = $twig;
         $this->formFactory = $formFactory;
+        $this->addTrickHandler = $addTrickHandler;
+        $this->urlGenerator = $urlGenerator;
     }
 
     /**
@@ -39,10 +62,17 @@ class AddTrickController
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
+     * @throws \Exception
      */
     public function add(Request $request): Response
     {
         $form = $this->formFactory->create(AddTrickType::class)->handleRequest($request);
+
+        if ($this->addTrickHandler->handle($form)) {
+            return new RedirectResponse(
+                $this->urlGenerator->generate('home')
+            );
+        }
 
         return new Response(
             $this->twig->render('app/CRUD/add.html.twig',[
