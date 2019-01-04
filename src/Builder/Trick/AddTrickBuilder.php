28cb085024d0c8c36a8a7c872381f55ddc59b67d
ludovicjj <jahanlud@gmail.com>
2019-01-04 05:21:50 +0100

    Create AddTrickBuilder, return new trick

---
+++ src/Builder/Trick/AddTrickBuilder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Builder\Trick;
+
+use App\Entity\Trick;
+use App\DTO\TrickDTO;
+
+class AddTrickBuilder
+{
+    /**
+     * @param TrickDTO $trickDTO
+     * @return Trick
+     * @throws \Exception
+     */
+    public function create(TrickDTO $trickDTO): Trick
+    {
+        return new trick(
+            $trickDTO->title,
+            $trickDTO->description,
+            $trickDTO->title,
+            $trickDTO->category
+        );
+    }
+}
