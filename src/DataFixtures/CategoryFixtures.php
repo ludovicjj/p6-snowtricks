2808023e77604c40c7a6cc3278268b58bbda784f
ludovicjj <jahanlud@gmail.com>
2019-01-04 01:30:20 +0100

    Update CategoryFixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,8 +2,33 @@
 
 namespace App\DataFixtures;
 
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Category;
 
-class CategoryFixtures
+class CategoryFixtures extends Fixture
 {
+    /**
+     * @param ObjectManager $manager
+     * @throws \Exception
+     */
+    public function load(ObjectManager $manager)
+    {
+        $names = [
+            'rotations',
+            'flips',
+            'airs',
+            'slides',
+            'freestyle',
+            'grabs'
+        ];
 
+        foreach($names as $name)
+        {
+            $category = new Category($name);
+
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
 }
