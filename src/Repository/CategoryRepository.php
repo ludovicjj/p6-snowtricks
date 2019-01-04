1b39eae6fce800de854c342ac8c3fe86534c123b
ludovicjj <jahanlud@gmail.com>
2019-01-04 01:02:29 +0100

    Update CategoryRepository

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -2,8 +2,14 @@
 
 namespace App\Repository;
 
+use App\Entity\Category;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 
-class CategoryRepository
+class CategoryRepository extends ServiceEntityRepository
 {
-
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Category::class);
+    }
 }
