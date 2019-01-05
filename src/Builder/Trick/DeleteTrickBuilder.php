a894e5ed8c2dc86d4992e2182487c7117b0fe79b
ludovicjj <jahanlud@gmail.com>
2019-01-06 00:32:01 +0100

    Delete all trick's images and remove trick

--- src/Builder/Trick/DeleteTrickBuilder.php
+++ src/Builder/Trick/DeleteTrickBuilder.php
@@ -3,6 +3,8 @@
 namespace App\Builder\Trick;
 
 use App\Entity\Trick;
+use App\Repository\TrickRepository;
+use App\Service\ImageDelete;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
@@ -14,19 +16,43 @@ class DeleteTrickBuilder
      */
     private $csrfTokenManager;
 
+    /**
+     * @var ImageDelete
+     */
+    private $imageDelete;
+
+    private $trickRepository;
+
     public function __construct(
-        CsrfTokenManagerInterface $csrfTokenManager
+        CsrfTokenManagerInterface $csrfTokenManager,
+        ImageDelete $imageDelete,
+        TrickRepository $trickRepository
     )
     {
         $this->csrfTokenManager = $csrfTokenManager;
+        $this->imageDelete = $imageDelete;
+        $this->trickRepository = $trickRepository;
     }
 
+    /**
+     * @param Trick $trick
+     * @param string $submittedToken
+     * @return bool
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
     public function delete(
         Trick $trick,
         string $submittedToken
     )
     {
         if ($this->csrfTokenManager->isTokenValid(new CsrfToken('delete'.$trick->getId()->toString(), $submittedToken))) {
+
+            foreach ($trick->getImages() as $image) {
+                $this->imageDelete->delete($image);
+            }
+
+            $this->trickRepository->remove($trick);
 
             return true;
         } else {
