ff21f0c49e7043ade9d2833d7d596826a01b3992
ludovicjj <jahanlud@gmail.com>
2019-01-05 23:35:43 +0100

    Find trick by slug, return RedirectResponse to home page

--- src/Controller/Trick/DeleteTrickController.php
+++ src/Controller/Trick/DeleteTrickController.php
@@ -2,8 +2,47 @@
 
 namespace App\Controller\Trick;
 
+use App\Repository\TrickRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 
 class DeleteTrickController
 {
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
 
+    private $trickRepository;
+
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        TrickRepository $trickRepository
+    )
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->trickRepository = $trickRepository;
+    }
+
+    /**
+     * @Route("/figure/supprimer/{slug}", name="delete_trick")
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function delete(Request $request): RedirectResponse
+    {
+        $trick = $this->trickRepository->findBy(['slug' => $request->attributes->get('slug')]);
+
+        if (!$trick) {
+            throw new NotFoundHttpException('Aucune figure ne correspond aux données reçues');
+        }
+
+        return new RedirectResponse(
+            $this->urlGenerator->generate('home')
+        );
+    }
 }
