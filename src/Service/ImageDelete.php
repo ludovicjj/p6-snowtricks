35773080daa5e3f0a44969e19d06515df28f2171
ludovicjj <jahanlud@gmail.com>
2019-01-06 00:15:10 +0100

    Delete image

--- src/Service/ImageDelete.php
+++ src/Service/ImageDelete.php
@@ -2,8 +2,31 @@
 
 namespace App\Service;
 
+use App\Entity\Image;
 
 class ImageDelete
 {
+    /**
+     * @var string
+     */
+    private $imageDirectory;
 
+    public function __construct(
+        string $imageDirectory
+    )
+    {
+        $this->imageDirectory = $imageDirectory;
+    }
+
+    /**
+     * @param Image $image
+     */
+    public function delete(Image $image)
+    {
+        $oldImage = $this->imageDirectory . '/' . $image->getFilename();
+
+        if (file_exists($oldImage)) {
+            unlink($oldImage);
+        }
+    }
 }
