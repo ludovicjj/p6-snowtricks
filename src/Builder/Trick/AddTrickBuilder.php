5f4a7c96dba718aa61cf6a248514918e88a66a8f
ludovicjj <jahanlud@gmail.com>
2019-01-05 01:11:49 +0100

    Update AddTrickBuilder with VideoBuilder

--- src/Builder/Trick/AddTrickBuilder.php
+++ src/Builder/Trick/AddTrickBuilder.php
@@ -4,9 +4,22 @@ namespace App\Builder\Trick;
 
 use App\Entity\Trick;
 use App\DTO\TrickDTO;
+use App\Builder\Video\VideoBuilder;
 
 class AddTrickBuilder
 {
+    /**
+     * @var VideoBuilder
+     */
+    private $videoBuilder;
+
+    public function __construct(
+        VideoBuilder $videoBuilder
+    )
+    {
+        $this->videoBuilder = $videoBuilder;
+    }
+
     /**
      * @param TrickDTO $trickDTO
      * @return Trick
@@ -18,7 +31,8 @@ class AddTrickBuilder
             $trickDTO->title,
             $trickDTO->description,
             $trickDTO->title,
-            $trickDTO->category
+            $trickDTO->category,
+            $this->videoBuilder->create($trickDTO->videos)
         );
     }
 }
