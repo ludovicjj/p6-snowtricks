d9a78387871137fa7872b182f9a8d9cbba6b3383
ludovicjj <jahanlud@gmail.com>
2019-01-06 02:06:37 +0100

    Hydrat field video with VideoDTOFactory, return TrickDTO

--- src/Factory/TrickDTOFactory.php
+++ src/Factory/TrickDTOFactory.php
@@ -7,13 +7,28 @@ use App\Entity\Trick;
 
 class TrickDTOFactory
 {
-    public function create(Trick $trick)
+    private $videoDTOFactory;
+
+    public function __construct(
+        VideoDTOFactory $videoDTOFactory
+    )
     {
+        $this->videoDTOFactory = $videoDTOFactory;
+    }
+
+    public function create(Trick $trick): TrickDTO
+    {
+        $videoDTO = [];
+
+        foreach ($trick->getVideos() as $video) {
+            $videoDTO[] = $this->videoDTOFactory->create($video);
+        }
+
         return new TrickDTO(
             $trick->getTitle(),
             $trick->getDescription(),
             $trick->getCategory(),
-            null,
+            $videoDTO,
             null
         );
     }
