61d6aebd35f3633146d5646103573fa29bf544a4
ludovicjj <jahanlud@gmail.com>
2019-01-06 22:53:44 +0100

    Create form

--- src/Controller/User/RegistrationUserController.php
+++ src/Controller/User/RegistrationUserController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\User;
 
 
+use App\Form\Type\RegistrationType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -10,25 +13,34 @@ use Twig\Environment;
 class RegistrationUserController
 {
     private $twig;
+    private $formFactory;
 
     public function __construct(
-        Environment $twig
+        Environment $twig,
+        FormFactoryInterface $formFactory
     )
     {
         $this->twig = $twig;
+        $this->formFactory = $formFactory;
     }
 
     /**
      * @Route("/inscription", name="security_registration")
+     * @param Request $request
      * @return Response
+     *
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function registration()
+    public function registration(Request $request)
     {
+        $form = $this->formFactory->create(RegistrationType::class)->handleRequest($request);
+
         return new Response(
-            $this->twig->render('app/User/registration.html.twig')
+            $this->twig->render('app/User/registration.html.twig', [
+                'form' => $form->createView()
+            ])
         );
     }
 }
