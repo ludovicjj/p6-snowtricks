f117ce56ab8957723f6052a5ad6f7881f62addf8
ludovicjj <jahanlud@gmail.com>
2019-01-06 06:08:50 +0100

    Init method Compare array VideoDTO and array VideoEntity

---
+++ src/Builder/Video/UpdateVideoBuilder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Builder\Video;
+
+
+class UpdateVideoBuilder
+{
+    /**
+     * @param array $videoDTO
+     * @param array $videoEntity
+     */
+    public function compare(array $videoDTO, array $videoEntity)
+    {
+
+    }
+}
