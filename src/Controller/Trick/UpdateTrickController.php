8dd16092e737a5d4eaa456bdf2fc1af5e48a7877
ludovicjj <jahanlud@gmail.com>
2019-01-06 01:42:16 +0100

    Find trick by slug, if trick is null throw exception

--- src/Controller/Trick/UpdateTrickController.php
+++ src/Controller/Trick/UpdateTrickController.php
@@ -2,31 +2,51 @@
 
 namespace App\Controller\Trick;
 
+use App\Repository\TrickRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 class UpdateTrickController
 {
+    /**
+     * @var Environment
+     */
     private $twig;
 
+    /**
+     * @var TrickRepository
+     */
+    private $trickRepository;
+
     public function __construct(
-        Environment $twig
+        Environment $twig,
+        TrickRepository $trickRepository
     )
     {
         $this->twig = $twig;
+        $this->trickRepository = $trickRepository;
     }
 
     /**
      * @Route("/figure/modifier/{slug}", name="update_trick")
+     * @param Request $request
      * @return Response
      *
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function update(): Response
+    public function update(Request $request): Response
     {
+        $trick = $this->trickRepository->findOneBy(['slug' => $request->attributes->get('slug')]);
+
+        if (!$trick) {
+            throw new NotFoundHttpException('Aucune figure ne correspond aux données reçues');
+        }
+
         return new Response(
             $this->twig->render('app/CRUD/update.html.twig')
         );
