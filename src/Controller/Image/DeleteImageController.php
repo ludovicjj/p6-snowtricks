77dbc1f18b6a19da05b98c9eb9741a52bcddd979
ludovicjj <jahanlud@gmail.com>
2019-01-06 03:58:23 +0100

    Find image by id

--- src/Controller/Image/DeleteImageController.php
+++ src/Controller/Image/DeleteImageController.php
@@ -2,8 +2,28 @@
 
 namespace App\Controller\Image;
 
+use App\Repository\ImageRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DeleteImageController
 {
+    private $imageRepository;
 
+    public function __construct(
+        ImageRepository $imageRepository
+    )
+    {
+        $this->imageRepository = $imageRepository;
+    }
+
+    /**
+     * @Route("/delete/image/{id_image}", methods="DELETE", name="delete_images")
+     * @param Request $request
+     */
+    public function delete(Request $request)
+    {
+        /* @var \App\Entity\Image $image */
+        $image = $this->imageRepository->findOneBy(['id' => $request->attributes->get('id_image')]);
+    }
 }
