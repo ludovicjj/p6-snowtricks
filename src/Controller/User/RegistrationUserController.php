a351115bb5c4d2a21a22d8ea1908dd022d652e0d
ludovicjj <jahanlud@gmail.com>
2019-01-06 22:33:14 +0100

    Return new response to template registration

--- src/Controller/User/RegistrationUserController.php
+++ src/Controller/User/RegistrationUserController.php
@@ -3,7 +3,32 @@
 namespace App\Controller\User;
 
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
 class RegistrationUserController
 {
+    private $twig;
 
+    public function __construct(
+        Environment $twig
+    )
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/inscription", name="security_registration")
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function registration()
+    {
+        return new Response(
+            $this->twig->render('app/User/registration.html.twig')
+        );
+    }
 }
