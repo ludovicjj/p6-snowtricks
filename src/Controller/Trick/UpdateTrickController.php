2d5bd4a4bb623a0526e493507e900013c69354cc
ludovicjj <jahanlud@gmail.com>
2019-01-06 02:49:34 +0100

    Create form with trickDTO hydrat by trick, render form to update template

--- src/Controller/Trick/UpdateTrickController.php
+++ src/Controller/Trick/UpdateTrickController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Trick;
 
+use App\Factory\TrickDTOFactory;
+use App\Form\Type\AddTrickType;
 use App\Repository\TrickRepository;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -21,13 +24,27 @@ class UpdateTrickController
      */
     private $trickRepository;
 
+    /**
+     * @var TrickDTOFactory
+     */
+    private $trickDTOFactory;
+
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
     public function __construct(
         Environment $twig,
-        TrickRepository $trickRepository
+        TrickRepository $trickRepository,
+        TrickDTOFactory $trickDTOFactory,
+        FormFactoryInterface $formFactory
     )
     {
         $this->twig = $twig;
         $this->trickRepository = $trickRepository;
+        $this->trickDTOFactory = $trickDTOFactory;
+        $this->formFactory = $formFactory;
     }
 
     /**
@@ -41,14 +58,21 @@ class UpdateTrickController
      */
     public function update(Request $request): Response
     {
+        /* @var \App\Entity\Trick $trick */
         $trick = $this->trickRepository->findOneBy(['slug' => $request->attributes->get('slug')]);
 
         if (!$trick) {
             throw new NotFoundHttpException('Aucune figure ne correspond aux données reçues');
         }
 
+        $trickDTO = $this->trickDTOFactory->create($trick);
+
+        $form = $this->formFactory->create(AddTrickType::class, $trickDTO)->handleRequest($request);
+
         return new Response(
-            $this->twig->render('app/CRUD/update.html.twig')
+            $this->twig->render('app/CRUD/update.html.twig',[
+                'form' => $form->createView()
+            ])
         );
     }
 }
