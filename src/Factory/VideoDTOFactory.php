8ea70340cd1a0d95a09d82235378644b021de483
ludovicjj <jahanlud@gmail.com>
2019-01-06 02:02:21 +0100

    Hydrat VideoDTO and return VideoDTO

--- src/Factory/VideoDTOFactory.php
+++ src/Factory/VideoDTOFactory.php
@@ -2,8 +2,19 @@
 
 namespace App\Factory;
 
+use App\DTO\VideoDTO;
+use App\Entity\Video;
 
 class VideoDTOFactory
 {
-
+    /**
+     * @param Video $video
+     * @return VideoDTO
+     */
+    public function create(Video $video): VideoDTO
+    {
+        return new VideoDTO(
+            $video->getUrl()
+        );
+    }
 }
