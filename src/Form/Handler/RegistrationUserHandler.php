889020e4895258bb819483dc924ad51276e8d8f9
ludovicjj <jahanlud@gmail.com>
2019-01-06 22:59:13 +0100

    Create method handle, return bool

--- src/Form/Handler/RegistrationUserHandler.php
+++ src/Form/Handler/RegistrationUserHandler.php
@@ -2,8 +2,21 @@
 
 namespace App\Form\Handler;
 
+use Symfony\Component\Form\FormInterface;
 
 class RegistrationUserHandler
 {
+    /**
+     * @param FormInterface $form
+     * @return bool
+     */
+    public function handle(FormInterface $form): bool
+    {
+        if ($form->isSubmitted() && $form->isValid()) {
 
+            return true;
+        }
+
+        return false;
+    }
 }
