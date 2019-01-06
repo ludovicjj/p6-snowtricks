89243ac13a0bcbef1fd590ea365f7ee346ee8e1c
ludovicjj <jahanlud@gmail.com>
2019-01-06 01:58:55 +0100

    Hydrat TrickDTO, field title, description, category

--- src/Factory/TrickDTOFactory.php
+++ src/Factory/TrickDTOFactory.php
@@ -2,8 +2,19 @@
 
 namespace App\Factory;
 
+use App\DTO\TrickDTO;
+use App\Entity\Trick;
 
 class TrickDTOFactory
 {
-
+    public function create(Trick $trick)
+    {
+        return new TrickDTO(
+            $trick->getTitle(),
+            $trick->getDescription(),
+            $trick->getCategory(),
+            null,
+            null
+        );
+    }
 }
