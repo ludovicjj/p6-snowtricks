ae6e375420c2f0068fa01aa9600766f5c84c5ecd
ludovicjj <jahanlud@gmail.com>
2019-01-06 22:01:34 +0100

    Create RegistrationDTO with username, email, password, confirmPassword

---
+++ src/DTO/RegistrationDTO.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DTO;
+
+class RegistrationDTO
+{
+    /**
+     * @var string
+     */
+    public $username;
+
+    /**
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @var string
+     */
+    public $password;
+
+    /**
+     * @var string
+     */
+    public $confirmPassword;
+}
