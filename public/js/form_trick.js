99cf3c4be78294ff7bc093ad1b7d73cff07332c8
ludovicjj <jahanlud@gmail.com>
2019-01-06 05:18:02 +0100

    remove container and display message success

--- public/js/form_trick.js
+++ public/js/form_trick.js
@@ -58,16 +58,20 @@ $(document).on("change", ":input[type=file]", function (e) {
 
 /* Evement click pour supprimer les image via Ajax */
 $(document).ready(function() {
-    $('.link-ajax').click(function(event) {
-        event.preventDefault();
-        var url = $(event.currentTarget).attr('data-url');
+    $(document).on('click', '.link-ajax', function(e){
+        e.preventDefault();
+        let url = $(this).attr('data-url');
         $.ajax({
             url: url,
             type: 'DELETE',
             dataType: 'JSON',
             success: function (response, statusCode, xhr) {
-                console.log(response.message)
+                let $messageDelete = $('.image-delete-message');
+                $messageDelete.text(response.message);
+                $messageDelete.show("slow").delay(3000).hide("slow");
             }
         });
+        let container = $(this).closest($(".js_image_ajax_container"));
+        container.remove();
     });
 });
