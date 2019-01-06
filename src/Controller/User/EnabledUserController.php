367bd0183d1102a797662038bd1119ae49ba1ebb
ludovicjj <jahanlud@gmail.com>
2019-01-07 00:30:23 +0100

    Find user by token, if user is null thow exception. Return RedirectReponse to HomeController

--- src/Controller/User/EnabledUserController.php
+++ src/Controller/User/EnabledUserController.php
@@ -2,7 +2,51 @@
 
 namespace App\Controller\User;
 
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 class EnabledUserController
 {
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
 
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    public function __construct(
+        UserRepository $userRepository,
+        UrlGeneratorInterface $urlGenerator
+    )
+    {
+        $this->userRepository = $userRepository;
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * @Route("/enabled/{token}", name="security_enabled")
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function enabled(Request $request): RedirectResponse
+    {
+        /* @var \App\Entity\User $user */
+        $user = $this->userRepository->findOneBy(['token' => $request->attributes->get('token')]);
+
+        if (!$user) {
+            throw new NotFoundHttpException('Aucun utilisateur ne correspond a ce token');
+        }
+
+        return new RedirectResponse(
+            $this->urlGenerator->generate('home')
+        );
+    }
 }
