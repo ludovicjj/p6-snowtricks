2de4527d45ac237572c0e2bc4b87b0faad846644
ludovicjj <jahanlud@gmail.com>
2019-01-06 23:22:07 +0100

    Create new User, hash password by encoder and default avatar. Return User

--- src/Builder/User/RegistrationUserBuilder.php
+++ src/Builder/User/RegistrationUserBuilder.php
@@ -2,8 +2,50 @@
 
 namespace App\Builder\User;
 
+use App\DTO\RegistrationDTO;
+use App\Entity\User;
+use App\Entity\Avatar;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationUserBuilder
 {
+    /**
+     * @var User
+     */
+    private $user;
 
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(
+        UserPasswordEncoderInterface $passwordEncoder
+    )
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @param RegistrationDTO $registrationDTO
+     * @return User
+     * @throws \Exception
+     */
+    public function registration(RegistrationDTO $registrationDTO): User
+    {
+        $this->user = new user();
+
+        $this->user->registrationUser(
+            $registrationDTO->username,
+            $this->passwordEncoder->encodePassword($this->user, $registrationDTO->password),
+            $registrationDTO->email,
+            new Avatar(
+                'default-avatar.png',
+                '/image/default-avatar.png',
+                'avatar-default'
+            )
+        );
+
+        return $this->user;
+    }
 }
