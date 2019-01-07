2139610a388f7479253742b596bda8a243233f50
ludovicjj <jahanlud@gmail.com>
2019-01-07 06:22:27 +0100

    if method handle from ResetPasswordHandler return true redirect to LoginUserController

--- src/Controller/User/ResetPasswordUserController.php
+++ src/Controller/User/ResetPasswordUserController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller\User;
 
+use App\Form\Handler\ResetPasswordHandler;
 use App\Form\Type\ResetType;
 use App\Repository\UserRepository;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Environment;
 
 class ResetPasswordUserController
@@ -28,15 +31,38 @@ class ResetPasswordUserController
      */
     private $twig;
 
+    /**
+     * @var ResetPasswordHandler
+     */
+    private $resetPasswordHandler;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * ResetPasswordUserController constructor.
+     * @param UserRepository $userRepository
+     * @param FormFactoryInterface $formFactory
+     * @param Environment $twig
+     * @param ResetPasswordHandler $resetPasswordHandler
+     * @param UrlGeneratorInterface $urlGenerator
+     */
     public function __construct(
         UserRepository $userRepository,
         FormFactoryInterface $formFactory,
-        Environment $twig
+        Environment $twig,
+        ResetPasswordHandler $resetPasswordHandler,
+        UrlGeneratorInterface $urlGenerator
     )
     {
         $this->userRepository = $userRepository;
         $this->formFactory = $formFactory;
         $this->twig = $twig;
+        $this->resetPasswordHandler = $resetPasswordHandler;
+        $this->urlGenerator = $urlGenerator;
+
     }
 
     /**
@@ -44,12 +70,15 @@ class ResetPasswordUserController
      * @param Request $request
      * @return Response
      *
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
     public function reset(Request $request): Response
     {
+        /* @var \App\Entity\User $user */
         $user = $this->userRepository->findOneBy(['token' => $request->attributes->get('token')]);
 
         if (!$user) {
@@ -57,6 +86,12 @@ class ResetPasswordUserController
         }
 
         $form = $this->formFactory->create(ResetType::class)->handleRequest($request);
+
+        if ($this->resetPasswordHandler->handle($form, $user)) {
+            return new RedirectResponse(
+                $this->urlGenerator->generate('security_login')
+            );
+        }
 
         return new Response(
             $this->twig->render('app/User/reset.html.twig', [
