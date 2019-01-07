fd9d114a5c3a6c37ec2cdb7dc955268ff61a89f8
ludovicjj <jahanlud@gmail.com>
2019-01-07 01:27:25 +0100

    Create Method login, return response

--- src/Controller/User/LoginUserController.php
+++ src/Controller/User/LoginUserController.php
@@ -2,8 +2,33 @@
 
 namespace App\Controller\User;
 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class LoginUserController
 {
+    private $twig;
 
+    public function __construct(
+        Environment $twig
+    )
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/connexion", name="security_login")
+     * @return Response
+     *
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function login(): Response
+    {
+        return new Response(
+            $this->twig->render('app/User/login.html.twig')
+        );
+    }
 }
