6ffaa5578b1d097e6782f5d4eea0bd32c3a1aa4e
ludovicjj <jahanlud@gmail.com>
2019-01-07 03:56:22 +0100

    Create method forgotten, return response

--- src/Controller/User/ForgottenPasswordUserController.php
+++ src/Controller/User/ForgottenPasswordUserController.php
@@ -2,8 +2,36 @@
 
 namespace App\Controller\User;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ForgottenPasswordUserController
 {
+    /**
+     * @var Environment
+     */
+    private $twig;
 
+    public function __construct(
+        Environment $twig
+    )
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/password", name="security_forgotten")
+     * @return Response
+     *
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function forgotten(): Response
+    {
+        return new Response(
+            $this->twig->render('app/User/forgotten.html.twig')
+        );
+    }
 }
