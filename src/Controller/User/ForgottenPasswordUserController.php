27e9819bc1ca6fd50fd8fb0d0dbbf18d90f5f1a5
ludovicjj <jahanlud@gmail.com>
2019-01-07 04:12:06 +0100

    Create form

--- src/Controller/User/ForgottenPasswordUserController.php
+++ src/Controller/User/ForgottenPasswordUserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\User;
 
+use App\Form\Type\ForgottenType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -13,25 +16,37 @@ class ForgottenPasswordUserController
      */
     private $twig;
 
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
     public function __construct(
-        Environment $twig
+        Environment $twig,
+        FormFactoryInterface $formFactory
     )
     {
         $this->twig = $twig;
+        $this->formFactory = $formFactory;
     }
 
     /**
      * @Route("/password", name="security_forgotten")
+     * @param Request $request
      * @return Response
      *
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function forgotten(): Response
+    public function forgotten(Request $request): Response
     {
+        $form = $this->formFactory->create(ForgottenType::class)->handleRequest($request);
+
         return new Response(
-            $this->twig->render('app/User/forgotten.html.twig')
+            $this->twig->render('app/User/forgotten.html.twig', [
+                'form' => $form->createView()
+            ])
         );
     }
 }
