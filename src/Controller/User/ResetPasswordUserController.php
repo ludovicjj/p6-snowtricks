be29a37972720f930617de72ef72adac0fa06dfe
ludovicjj <jahanlud@gmail.com>
2019-01-07 04:50:51 +0100

    Find user by token, if user is null throw exception

--- src/Controller/User/ResetPasswordUserController.php
+++ src/Controller/User/ResetPasswordUserController.php
@@ -2,8 +2,32 @@
 
 namespace App\Controller\User;
 
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ResetPasswordUserController
 {
+    private $userRepository;
 
+    public function __construct(
+        UserRepository $userRepository
+    )
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @Route("/reinitialiser/{token}", name="security_reset")
+     * @param Request $request
+     */
+    public function reset(Request $request)
+    {
+        $user = $this->userRepository->findOneBy(['token' => $request->attributes->get('token')]);
+
+        if (!$user) {
+            throw new NotFoundHttpException('Aucun utilisateur ne correspond a ce token');
+        }
+    }
 }
