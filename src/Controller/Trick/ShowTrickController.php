1ef72cef99fd80eb3bbcceaff568437c6997878c
ludovicjj <jahanlud@gmail.com>
2019-01-07 11:38:36 +0100

    If method handle from AddCommentHandler return true return redirect response to ShowTrickController for refresh comment form

--- src/Controller/Trick/ShowTrickController.php
+++ src/Controller/Trick/ShowTrickController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller\Trick;
 
+use App\Form\Handler\AddCommentHandler;
 use App\Form\Type\CommentType;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TrickRepository;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Environment;
 
 class ShowTrickController
@@ -28,15 +31,29 @@ class ShowTrickController
      */
     private $formFactory;
 
+    /**
+     * @var AddCommentHandler
+     */
+    private $addCommentHandler;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
     public function __construct(
         TrickRepository $trickRepository,
         Environment $twig,
-        FormFactoryInterface $formFactory
+        FormFactoryInterface $formFactory,
+        AddCommentHandler $addCommentHandler,
+        UrlGeneratorInterface $urlGenerator
     )
     {
         $this->trickRepository = $trickRepository;
         $this->twig = $twig;
         $this->formFactory = $formFactory;
+        $this->addCommentHandler = $addCommentHandler;
+        $this->urlGenerator = $urlGenerator;
     }
 
     /**
@@ -58,6 +75,13 @@ class ShowTrickController
         }
 
         $form = $this->formFactory->create(CommentType::class)->handleRequest($request);
+
+        if ($this->addCommentHandler->handle($form, $trick)) {
+
+            return new RedirectResponse(
+                $this->urlGenerator->generate('show_trick', ['slug' => $trick->getSlug()])
+            );
+        }
 
         return new Response(
             $this->twig->render('app/CRUD/show.html.twig', [
